cc31af4f1b5b14fdb6c897753f3d2dacc8f0641f
Rutagengwa Eric <rutagebanke@gmail.com>
2025-02-17 10:46:24 +0200

    adding boxes

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -3,7 +3,24 @@ const TicTacToe = () => {
   return (
     <div className="container">
         <h1 className="title"> Tic Tac Toe in <span>React</span> </h1>
-      <div className="board"></div>
+      <div className="board">
+          <div className="row1">
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+          </div>
+          <div className="row2">
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+          </div>
+          <div className="row3">
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+                <div className="boxes"> </div>
+          </div>
+          
+      </div>
       <button className="reset"> Reset </button>
     </div>
   ) 
